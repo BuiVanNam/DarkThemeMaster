d783ffb936872393f86789b3c9fbcc56fa08cf07
BuiVanNam <buinam1401.nb@gmail.com>
2020-10-05 22:40:15 +0700

    setup project

---
+++ app/src/main/java/com/nambv/demo/darkthememaster/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nambv.demo.darkthememaster.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nambv.demo.darkthememaster.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/nambv/demo/darkthememaster/utils/Constant.kt
@@ -0,0 +1,5 @@
+package com.nambv.demo.darkthememaster.utils
+
+/**
+ * Created by nambv on 10/5/2020
+ */
